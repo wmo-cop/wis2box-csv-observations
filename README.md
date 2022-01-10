3757cf36d4425ea26aa5d7c79ac81c22c8d83fc1
Tom Kralidis <tomkralidis@gmail.com>
2022-01-10 18:01:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# wis2node-malawi-observations
+
+## Overview
+
+wis2node Malawi observations data ingest plugin
+
+## Installation
+
+The easiest way to install wis2node-malawi-observations is via the Python [pip](https://pip.pypa.io/en/stable/)
+utility:
+
+```bash
+# via PyPI
+pip install wis2node-malawi-observations
+
+# directly from GitHub
+pip install https://github.com/wmo-cop/wis2node-malawi-observations/archive/main.zip
+```
+
+### Requirements
+- Python 3
+- [virtualenv](https://virtualenv.pypa.io/)
+
+### Dependencies
+Dependencies are listed in [requirements.txt](requirements.txt). Dependencies
+are automatically installed during wis2node-malawi-observations installation.
+
+### Installing wis2node-malawi-observations
+
+```bash
+# setup virtualenv
+python3 -m venv --system-site-packages wis2node-malawi-observations
+cd wis2node-malawi-observations
+source bin/activate
+
+# clone codebase and install
+git clone https://github.com/wmo-cop/wis2node-malawi-observations.git
+cd wis2node-malawi-observations
+python setup.py build
+python setup.py install
+```
+
+## Running
+
+- install into wis2node container/environment
+- update WIS2NODE_DATADIR_DATA_MAPPINGS (see sample data-mappings.yml)
+
+
+## Development
+
+### Running Tests
+
+```bash
+# install dev requirements
+pip install -r requirements-dev.txt
+
+# run tests like this:
+python tests/run_tests.py
+
+# or this:
+python setup.py test
+```
+
+## Releasing
+
+```bash
+python setup.py sdist bdist_wheel --universal
+twine upload dist/*
+```
+
+### Code Conventions
+
+* [PEP8](https://www.python.org/dev/peps/pep-0008)
+
+### Bugs and Issues
+
+All bugs, enhancements and issues are managed on [GitHub](https://github.com/wmo-cop/wis2node-malawi-observations/issues).
+
+## Contact
+
+* Firstname Lastname
---
+++ tests/run_tests.py
@@ -0,0 +1,56 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
+import os
+import unittest
+
+TESTDATA_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_abspath(filepath):
+    """helper function to facilitate absolute test file access"""
+
+    return os.path.join(TESTDATA_DIR, filepath)
+
+
+def msg(test_id, test_description):
+    """convenience function to print out test id and desc"""
+    return '{}: {}'.format(test_id, test_description)
+
+
+class MalawiObservationsTest(unittest.TestCase):
+    """Test suite for package wis2node_malawi_observations"""
+    def setUp(self):
+        """setup test fixtures, etc."""
+        print(msg(self.id(), self.shortDescription()))
+
+    def tearDown(self):
+        """return to pristine state"""
+        pass
+
+    def test_smoke(self):
+        """Simple Smoke Test"""
+        # test assertions go here
+        self.assertEquals(1, 1, 'Expected equality')
+
+
+if __name__ == '__main__':
+    unittest.main()
